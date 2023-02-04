ee870108ad1f4880603969bc3e1402d19a461f28
Sankalpa Dutta <124279362+SankalpaDutta012@users.noreply.github.com>
2023-02-04 10:40:59 +0000

    first commit

---
+++ Assignment.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a , b;
+    printf("Enter a number:");
+    scanf("%d", &a);
+    b = ((a/100)*100) + (((a%100)/10)*2)*10 + (a%10);
+    printf("The new number is %d.\n", b);
+    return 0;
+}
